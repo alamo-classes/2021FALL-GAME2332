56042bff4a8875e3dce9ef03016ba0a7268ddfeb
Reckhammer <joshua.rechkemmer48@gmail.com>
2021-10-12 17:29:24 -0500

    Created GravityBody script
    NOTE: MOB BRANCH IS MISSING FROM MAIN

---
+++ IneptHillBillyFromSpace/Assets/Scripts/GravityBody.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityBody : MonoBehaviour
+{
+    public  GravityAttractor    attractor;      //The obj that THIS will be attracted to gravitationally    
+    public  RigidBody           rigidBody;      //The Rigidbody component of THIS game obj
+
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if ( attractor != null && rigidBody != null )
+            attractor.attract( rigidBody );
+    }
+}
