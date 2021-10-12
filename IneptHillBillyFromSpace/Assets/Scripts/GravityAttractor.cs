896d6b35b3ca371882f3e0801ed981ed9c57ef12
Reckhammer <joshua.rechkemmer48@gmail.com>
2021-10-12 17:51:06 -0500

    Added some helper functions to GravityAttractor

--- IneptHillBillyFromSpace/Assets/Scripts/GravityAttractor.cs
+++ IneptHillBillyFromSpace/Assets/Scripts/GravityAttractor.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class GravityAttractor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public  Vector3     gravCenter          = Vector3.zero;     //Center of the attractor's body
+    public  float       gravConstaint       = -9.8f;            //The force the attractor pulls bodies towards it
+    public  bool        weakenByDistance    = false;            //Does the pull weaken the farther the bodies is
+    public  float       attractorMass       = 100f;             //Mass of the attractor
+
+    void Awake()
     {
-        
+        //getComponent<RigidBody>().mass;
     }
 
-    // Update is called once per frame
-    void Update()
+    Vector3 findSurface( RigidBody attractedBody )
     {
-        
+        float distance = Vector3.Distance( this.transform.position, attractedBody.transform.position );
+        Vector3 surfaceNorm = Vector3.zero;
+
+        Raycast hit;
+        if ( Physics.Raycast( attractedBody.transform.position, attractedBody.transform.up, out hit, distance ) )
+        {
+            surfaceNorm = hit.normal;
+        }
+
+        return surfaceNorm;
+    }
+
+    //Aligns the attracted gravityBody's rotation to the surface of the attractor's body
+    void orientBody( RigidBody attractedBody, Vector3 surfaceNorm )
+    {
+        attractedBody.transform.localRotation = Quaternion.FromToRotation( attractedBody.transform.up, surfaceNorm ) * attractedBody.rotation;
     }
 }
