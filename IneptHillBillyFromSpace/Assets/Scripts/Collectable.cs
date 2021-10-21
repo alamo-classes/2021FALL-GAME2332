e524b249974739a666cd27bd62868d67bb0d03f8
adonjuan <adonjuan@student.alamo.edu>
2021-10-21 14:36:28 -0500

    Tested ticket/Added script in other Scene.
    Tested Ticket #16
    
    Added Script to be attached to Collectables so they disappear.
    -Tested functionality in CollectablesTest Scene

---
+++ IneptHillBillyFromSpace/Assets/Scripts/Collectable.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        Destroy(this.gameObject);
+        Debug.Log("Deleted " + gameObject.name);
+    }
+}
