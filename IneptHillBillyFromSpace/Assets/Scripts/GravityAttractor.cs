1d45fac134afd973765ff8dfcb6e56a2fe864d31
Reckhammer <joshua.rechkemmer48@gmail.com>
2021-10-26 20:08:24 -0500

    Player now moves relative to camera

--- IneptHillBillyFromSpace/Assets/Scripts/GravityAttractor.cs
+++ IneptHillBillyFromSpace/Assets/Scripts/GravityAttractor.cs
@@ -12,28 +12,48 @@ public class GravityAttractor : MonoBehaviour
 {
     public  float       gravForce           = -9.8f;            //The force the attractor pulls bodies towards it
 
-    void Awake()
+    //============================================================================================================
+    // Finds the surfaceNorm relative to the attractedBody's position and the attractor's position
+    //   and returns it as a Vector3
+    //
+    //   param attractedBody - RigidBody component of the gameObj. that we want to find the norm from
+    //
+    //   return surfaceNorm - Vector3 the surfaceNorm relative to attractedBody
+    //
+    public Vector3 findSurfaceNorm( Rigidbody attractedBody )
     {
-        //attractorMass = getComponent<RigidBody>().mass;
+        //Get the upward direction based from the position of the body and the center of the attractor
+        Vector3 surfaceNorm = ( attractedBody.transform.position - this.transform.position ).normalized;
+
+        return surfaceNorm;
     }
 
-    //Aligns the attracted gravityBody's rotation to the surface norm of the attractor's body
+    //============================================================================================================
+    // Aligns the attracted gravityBody's rotation to the surface norm of the attractor's body
+    // 
+    //   param attractedBody - RigidBody component of the gameObj. that we want to orient to the attractor
+    //   param surfaceNorm - Vector3 the surface norm relative to the attractedBody's position on the attractor
+    //
     public void orientBody( Rigidbody attractedBody, Vector3 surfaceNorm )
     {
-         Vector3 bodyUp = attractedBody.transform.up; //Get the upward direction based on the body
+        Vector3 bodyUp = attractedBody.transform.up; //Get the upward direction based on the body
+        
+        //Get the Rotation that we want to orient the body to
+        Quaternion targetRotation = Quaternion.FromToRotation( bodyUp, surfaceNorm ) * attractedBody.transform.rotation;
+        attractedBody.transform.rotation = Quaternion.Slerp( attractedBody.transform.rotation, targetRotation,
+              50 * Time.deltaTime ); //Rotate the body to the targetRotation over time
+    }
 
-         //Get the Rotation that we want to orient the body to
-         Quaternion targetRotation = Quaternion.FromToRotation( bodyUp, surfaceNorm ) * attractedBody.transform.rotation;
-         attractedBody.transform.rotation = Quaternion.Slerp( attractedBody.transform.rotation, targetRotation,
-               50 * Time.deltaTime ); //Rotate the body to the targetRotation over time
-   }
-
-    //Pulls the body towards the center of this gameObj.
-    //  Called by the GravityBody script
+    //=============================================================================================================
+    // Pulls the attractedBody towards the center of this gameObj.
+    //   Called by the GravityBody script
+    //
+    //   param attractedBody - Rigidbody component of gameObj. that we want to pull towards the center of attractor
+    //
     public void attract( Rigidbody attractedBody )
     {
         //Get the upward direction based from the position of the body and the center of the attractor
-        Vector3 surfaceNorm = (attractedBody.transform.position - this.transform.position).normalized;
+        Vector3 surfaceNorm = findSurfaceNorm(attractedBody);
 
         attractedBody.AddForce( surfaceNorm * gravForce ); //Pull the body towards the attractor
 
--- IneptHillBillyFromSpace/Assets/Scripts/PlayerMovement.cs
+++ IneptHillBillyFromSpace/Assets/Scripts/PlayerMovement.cs
@@ -11,16 +11,17 @@ public class PlayerMovement : MonoBehaviour
     public  float       jumpHeight  = 5f;     //How high the player jumps
     public  LayerMask   jumpSurfaces;         //Layer masks that tag which gameObjects can be jumped on
     private bool        isGrounded  = true;   //Boolean indicator telling if player is on the ground
-    private Transform   groundChecker = null;        //Child gameObj that tells if the player is on the ground
+    private Transform   groundChecker = null; //Child gameObj that tells if the player is on the ground
 
     private Rigidbody   rigidBody;      //Reference to the player's rigidbody component
+    private GravityBody gravBody;       //Reference to the player's GravityBody component
     private Vector3     inputs;         //Storage for the player's input
-    private Vector3 v;
 
 
     void Awake()
     {
         rigidBody = GetComponent<Rigidbody>();  //Get the reference to the rigidbody component
+        gravBody = GetComponent<GravityBody>(); //Get the reference to the GravityBody component
         inputs = Vector3.zero;                  //Set the vector to zero
         groundChecker = transform.Find( "GroundChecker" ).GetComponent<Transform>(); //Get the reference to the groundChecker child gameObj.'s Transform comp.
     }
@@ -32,39 +33,49 @@ public class PlayerMovement : MonoBehaviour
         isGrounded = Physics.CheckSphere( groundChecker.position, .2f, jumpSurfaces, QueryTriggerInteraction.Ignore );
 
         //Set the vector to the Player's press of WASD
-        //Vector3 movementX = Input.GetAxis("Horizontal");* Camera.main.transform.right;
-        //Vector3 movementZ = Input.GetAxis("Vertical"); * Camera.main.transform.forward;
+        Vector3 movementX = Input.GetAxis("Horizontal") * Camera.main.transform.right;
+        Vector3 movementZ = Input.GetAxis("Vertical") * Camera.main.transform.forward;
 
-        //inputs = movementX + movementZ;
-        inputs = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-
-        //v = transform.rotation.eulerAngles;                 // stops player object from leaning with Camera
-        //transform.rotation = Quaternion.Euler(0, v.y, 0);
+        inputs = movementX + movementZ;
+        //inputs = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
         ////Rotate the player to face the direction theyre facing
-        //if ( inputs != Vector3.zero)
-        //    transform.forward = inputs;
-
+        //if ( inputs != Vector3.zero )
+        //{
+        //   transform.forward = inputs;
+        //   orientBody();
+        //}
+        
         //If the player hits the jump button AND is on the ground,
         //    Launch them into the air
-        if ( Input.GetButtonDown("Jump") && isGrounded )
-           rigidBody.AddForce( transform.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange );
+        if ( Input.GetButtonDown( "Jump" ) && isGrounded )
+           rigidBody.AddForce( transform.up * Mathf.Sqrt( jumpHeight * -2f * Physics.gravity.y ), ForceMode.VelocityChange );
         
         //If the player holds down the sprint button,
         //    Set their speed to sprintSpeed
-        if ( Input.GetButtonDown("Sprint") )
+        if ( Input.GetButtonDown( "Sprint" ) )
            moveSpeed = sprintSpeed;
         
         //If the player let go of sprint button,
         //    Set their speed back to walkSpeed
-        if ( Input.GetButtonUp("Sprint") )
+        if ( Input.GetButtonUp( "Sprint" ) )
            moveSpeed = walkSpeed;
+        
 
    }
 
     void FixedUpdate()
     {
         //Using the inputs vector actually move the player
-        rigidBody.MovePosition( rigidBody.position + transform.TransformDirection( inputs ) * moveSpeed * Time.fixedDeltaTime );
+        rigidBody.MovePosition( rigidBody.position + inputs * moveSpeed * Time.fixedDeltaTime );
+    }
+
+    void orientBody()
+    {
+      if ( gravBody.attractor != null )
+      {
+         Vector3 surfaceNorm = gravBody.attractor.findSurfaceNorm( rigidBody );
+         gravBody.attractor.orientBody( rigidBody, surfaceNorm );
+      }
     }
 }
