312af45113e59211ea484c1a56fbc9bf3a452f4f
Gary <gsheridan4@student.alamo.edu>
2021-11-09 13:55:05 -0600

    changed the color of the planet
    made the o2 last longer on the player

--- IneptHillBillyFromSpace/Assets/Scripts/Planet/Planet.cs
+++ IneptHillBillyFromSpace/Assets/Scripts/Planet/Planet.cs
@@ -55,6 +55,8 @@ public class Planet : MonoBehaviour
                 meshObj.AddComponent<MeshRenderer>();
                 meshFilters[i] = meshObj.AddComponent<MeshFilter>();
                 meshFilters[i].sharedMesh = new Mesh();
+                //MeshCollider meshc = meshFilters[i].gameObject.AddComponent(typeof(MeshCollider)) as MeshCollider;
+                //meshc.sharedMesh = meshObj.GetComponent<Mesh>;
             }
             meshFilters[i].GetComponent<MeshRenderer>().sharedMaterial = colourSettings.planetMaterial;
 
